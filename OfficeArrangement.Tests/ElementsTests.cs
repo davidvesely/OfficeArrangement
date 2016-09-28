13a9dc0515f20fcea3e6e80e39d0e8275adb0531
davidvesely <davidvesely@gmail.com>
2016-09-29 01:03:48 +0300

    Some tests

--- OfficeArrangement.Tests/ElementsTests.cs
+++ OfficeArrangement.Tests/ElementsTests.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Test.VisualVerification;
+using System.Drawing;
+using OfficeArrangament.Common;
 
 namespace OfficeArrangement.Tests
 {
@@ -11,7 +14,29 @@ namespace OfficeArrangement.Tests
         [TestMethod]
         public void FreeSpace_filled_with_white()
         {
-            
+            int width = 250;
+            Rectangle rectangle = new Rectangle(0, 0, width, width);
+            Bitmap expectedBmp = new Bitmap(250, 250);
+            using (Graphics graphics = Graphics.FromImage(expectedBmp))
+            {
+                Brush brush = new SolidBrush(Color.White);
+                graphics.FillRectangle(brush, rectangle);
+            }
+            Snapshot expected = Snapshot.FromBitmap(expectedBmp);
+
+            // Act
+            FreeSpace freeSpace = new FreeSpace();
+            Bitmap actualBmp = freeSpace.Draw(width);
+            Snapshot actual = Snapshot.FromBitmap(actualBmp);
+
+            // Assert
+            Snapshot difference = actual.CompareTo(expected);
+            // Difference should be black image with no tollerance
+            SnapshotVerifier verifier = new SnapshotColorVerifier(Color.Black, new ColorDifference());
+            if (verifier.Verify(difference) == VerificationResult.Fail)
+            {
+                Assert.Fail("Expected image is not white");
+            }
         }
     }
 }
